bc0b16489b0c3285c455744403efa80e8c5102aa
Passsmpaas <lokeshsingh17628@gmail.com>
2025-03-26 19:24:21 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Timetrvlt
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/BrhamaKumar/idpasstotxtnew)
+                     
---
+++ config.py
@@ -0,0 +1,14 @@
+from os import getenv
+
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://fiona171593:tbGMvepmKQ8YNfJy@cluster0.5ccbrkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", ""))
+
+
